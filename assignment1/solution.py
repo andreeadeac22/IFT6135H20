f4f2772c61d5d281cff99b86770df10b0a563ed7
Andreea Deac <andreeadeac22@gmail.com>
2020-02-10 10:28:49 -0500

    first commit

--- assignment1/solution.py
+++ assignment1/solution.py
@@ -40,7 +40,7 @@ class NN(object):
         self.n_hidden = len(hidden_dims)
         self.lr = lr
         self.batch_size = batch_size
-        self.init_method = init_method
+        #self.init_method = init_method
         self.seed = seed
         self.activation_str = activation
         self.epsilon = epsilon
@@ -66,73 +66,90 @@ class NN(object):
         # self.weights is a dictionnary with keys W1, b1, W2, b2, ..., Wm, Bm where m - 1 is the number of hidden layers
         all_dims = [dims[0]] + list(self.hidden_dims) + [dims[1]]
         for layer_n in range(1, self.n_hidden + 2):
-            # WRITE CODE HERE
+            sd = np.sqrt(6.0 / (all_dims[layer_n-1] + all_dims[layer_n]))
+            self.weights[f"W{layer_n}"] = np.random.uniform(-sd, sd, (all_dims[layer_n-1], all_dims[layer_n]))
             self.weights[f"b{layer_n}"] = np.zeros((1, all_dims[layer_n]))
 
     def relu(self, x, grad=False):
         if grad:
-            # WRITE CODE HERE
-            pass
-        # WRITE CODE HERE
-        pass
-        return 0
+            return np.where(x <= 0, 0, 1)
+        return np.maximum(0, x)
 
     def sigmoid(self, x, grad=False):
         if grad:
-            # WRITE CODE HERE
+            return (1/1+np.exp(-x))*(1-1/1+np.exp(-x))
             pass
-        # WRITE CODE HERE
-        pass
-        return 0
+        return 1/1+np.exp(-x)
 
     def tanh(self, x, grad=False):
         if grad:
-            # WRITE CODE HERE
+            return 1-(np.tanh(x)*np.tanh(x))
             pass
-        # WRITE CODE HERE
-        pass
-        return 0
+        return np.tanh(x)
 
     def activation(self, x, grad=False):
         if self.activation_str == "relu":
-            # WRITE CODE HERE
-            pass
+            return self.relu(x, grad)
         elif self.activation_str == "sigmoid":
-            # WRITE CODE HERE
+            return self.relu(x, grad)
             pass
         elif self.activation_str == "tanh":
-            # WRITE CODE HERE
+            return self.tanh(x, grad)
             pass
         else:
             raise Exception("invalid")
-        return 0
 
     def softmax(self, x):
         # Remember that softmax(x-C) = softmax(x) when C is a constant.
-        # WRITE CODE HERE
-        pass
-        return 0
+        e_x = np.exp(x - np.max(x))
+        if len(x.shape) > 1\
+                :
+            return e_x / e_x.sum(axis=1, keepdims=True)
+        else:
+            return e_x / e_x.sum(axis=0, keepdims=True)
 
     def forward(self, x):
         cache = {"Z0": x}
-        # cache is a dictionnary with keys Z0, A0, ..., Zm, Am where m - 1 is the number of hidden layers
+        # cache is a dictionary with keys Z0, A0, ..., Zm, Am where m - 1 is the number of hidden layers
         # Ai corresponds to the preactivation at layer i, Zi corresponds to the activation at layer i
-        # WRITE CODE HERE
-        pass
+        for layer_n in range(1, self.n_hidden+1):
+            cache[f"A{layer_n}"] = np.matmul(cache[f"Z{layer_n-1}"], self.weights[f"W{layer_n}"]) + self.weights[f"b{layer_n}"]
+            cache[f"Z{layer_n}"] = self.activation(cache[f"A{layer_n}"])
+        cache[f"A{self.n_hidden + 1}"] = np.matmul(cache[f"Z{self.n_hidden }"], self.weights[f"W{self.n_hidden+1}"]) \
+                                         + self.weights[f"b{self.n_hidden+1}"]
+        cache[f"Z{self.n_hidden + 1}"] = self.softmax(cache[f"A{self.n_hidden + 1}"])
         return cache
 
     def backward(self, cache, labels):
         output = cache[f"Z{self.n_hidden + 1}"]
         grads = {}
-        # grads is a dictionnary with keys dAm, dWm, dbm, dZ(m-1), dA(m-1), ..., dW1, db1
-        # WRITE CODE HERE
-        pass
+        # grads is a dictionary with keys dAm, dWm, dbm, dZ(m-1), dA(m-1), ..., dW1, db1
+
+        # number of examples
+        n = len(labels)
+
+        grads[f"dA{self.n_hidden + 1}"] = output - labels.T
+        grads[f"dW{self.n_hidden + 1}"] = (1. / n) * np.matmul(grads[f"dA{self.n_hidden + 1}"],
+                                                               cache[f"Z{self.n_hidden}"])
+        grads[f"db{self.n_hidden + 1}"] = (1. / n) * np.sum(grads[f"dA{self.n_hidden + 1}"], axis=1, keepdims=True)
+
+
+        for layer_n in range(self.n_hidden, 0, -1):
+            grads[f"dZ{layer_n}"] = np.dot(self.weights[f"W{layer_n+1}"].T, grads[f"dA{layer_n+1}"])
+            grads[f"dA{layer_n}"] = grads[f"dZ{layer_n}"] * \
+                                           self.activation(cache[f"A{layer_n}"], grad=True)
+
+            grads[f"dW{layer_n}"] =(1. / n) * np.matmul(grads[f"dA{layer_n}"], cache[f"Z{layer_n - 1}"].T)
+            grads[f"db{layer_n}"] = (1. / n) * np.sum(grads[f"dA{layer_n}"], axis=1, keepdims=True)
+
         return grads
+
 
     def update(self, grads):
         for layer in range(1, self.n_hidden + 2):
-            # WRITE CODE HERE
-            pass
+            self.weights[f"W{layer}"] = self.weights[f"W{layer}"] - self.lr * grads[f"dW{layer}"]
+            self.weights[f"b{layer}"] = self.weights[f"b{layer}"] - self.lr * grads[f"db{layer}"]
+
 
     # def one_hot(self, y, n_classes=None):
     #     n_classes = n_classes or self.n_classes
